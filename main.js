6cce4437474b7c6db5968aa5882f7cff83ec2b4f
FRANPPP54 <pernaletefp@gmail.com>
2026-08-13 14:24:56 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,4 @@
+// Este código verifica que la app esté viva y conectada
+const statusEl = document.getElementById('status');
+statusEl.innerText = "¡Sistema conectado! Listo para empezar a traer datos.";
+console.log("Archivo main.js cargado correctamente.");
